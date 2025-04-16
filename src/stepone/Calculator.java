0d8175fedb8300415042c5f47259ba82cca8909d
박성현 <110206842+shPark95@users.noreply.github.com>
2025-04-16 21:27:52 +0900

    Add: Feature/calculate answer

--- src/stepone/Calculator.java
+++ src/stepone/Calculator.java
@@ -13,7 +13,22 @@ public class Calculator implements MenuBtn {
 
     @Override
     public void run() {
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int num1 = scanner.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = scanner.nextInt();
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        String operator = scanner.nextLine();
 
+        String result = calculate(num1, num2, operator.charAt(0));
+        System.out.println("결과: " + result);
+
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        String answer = scanner.nextLine();
+
+        if (answer.equals("exit")) {
+            exitBtn();
+        }
     }
     @Override
     public void exitBtn() {
@@ -23,4 +38,25 @@ public class Calculator implements MenuBtn {
     public void changeCalculator() {
 
     }
+
+    public String calculate(int num1, int num2, char operator) {
+        int result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                return Integer.toString(result);
+            case '-':
+                result = num1 - num2;
+                return Integer.toString(result);
+            case '*':
+                result = num1 * num2;
+                return Integer.toString(result);
+            case '/':
+                result = num1 / num2;
+                return Integer.toString(result);
+            default:
+                return "잘못된 연산자입니다.";
+        }
+    }
+
 }
