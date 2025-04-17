15a0beb3d0f248d614b55247869217bf936ab80d
박성현 <110206842+shPark95@users.noreply.github.com>
2025-04-17 12:13:27 +0900

    Add: Feature/getter + setter

--- src/steptwo/Calculator.java
+++ src/steptwo/Calculator.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 
 public class Calculator implements MenuBtn, ExceptionThrower {
     private Scanner scanner;
+    private int num1, num2;
+    private char operator;
 
     public Calculator(Scanner scanner) {
         this.scanner = scanner;
@@ -15,7 +17,16 @@ public class Calculator implements MenuBtn, ExceptionThrower {
 
     @Override
     public void run() {
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        setNum1(Integer.parseInt(scanner.nextLine()));
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        setNum2(Integer.parseInt(scanner.nextLine()));
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        String operator = scanner.nextLine();
+        setOperator(operator);
 
+        String result = calculate();
+        System.out.println("결과: " + result);
     }
     @Override
     public boolean exitBtn() {
@@ -37,4 +48,49 @@ public class Calculator implements MenuBtn, ExceptionThrower {
         }
     }
 
+    public String calculate() {
+        int result;
+        try {
+            switch (getOperator()) {
+                case '+':
+                    result = getNum1() + getNum2();
+                    return Integer.toString(result);
+                case '-':
+                    result = getNum1() - getNum2();
+                    return Integer.toString(result);
+                case '*':
+                    result = getNum1() * getNum2();
+                    return Integer.toString(result);
+                case '/':
+                    checknum2(getNum2());
+                    result = getNum1() / getNum2();
+                    return Integer.toString(result);
+                default:
+                    return "잘못된 연산자입니다.";
+            }
+        }
+        catch (UserException.DivZeroException e) {
+            return e.getMessage();
+        }
+        catch (Exception e) {
+            return "계산실패";
+        }
+    }
+
+    int getNum1() { return num1; }
+    void setNum1(int num1) {
+        this.num1 = num1;
+    }
+    int getNum2() {
+        return num2;
+    }
+    void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    char getOperator() {
+        return operator;
+    }
+    void setOperator(String operator) {
+        this.operator = operator.charAt(0);
+    }
 }
